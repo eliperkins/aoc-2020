edb5e999ef71a5ca6b2d4d9c1c16a81068528e7c
Eli Perkins <eli.j.perkins@gmail.com>
2020-12-01 14:31:57 -0500

    Simplify things

--- Scripts/ubuntu.sh
+++ Scripts/ubuntu.sh
@@ -5,14 +5,12 @@ if ! command -v docker > /dev/null; then
     exit 1
 fi
 action=$1
-swift=$2
-ubuntu=$3
 dockerfile=$(mktemp)
-echo "FROM swift:$swift-$ubuntu"    >  $dockerfile
-echo 'ADD . aoc-2020'               >> $dockerfile
-echo 'WORKDIR aoc-2020'             >> $dockerfile
-echo "RUN swift $action"            >> $dockerfile
-image=drstring
+echo "FROM swift:5.3.1-focal"   >  $dockerfile
+echo 'ADD . aoc-2020'           >> $dockerfile
+echo 'WORKDIR aoc-2020'         >> $dockerfile
+echo "RUN swift $action"        >> $dockerfile
+image=aoc-2020
 docker image rm -f "$image" > /dev/null
 docker build -t "$image" -f $dockerfile .
 docker run --rm "$image"
