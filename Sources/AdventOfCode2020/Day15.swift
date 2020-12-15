01e86db65093db7f119b5af6f6608cc6691dbf5c
Eli Perkins <eli.j.perkins@gmail.com>
2020-12-15 11:37:43 -0500

    Use tuple instead of array

--- Sources/AdventOfCode2020/Day15.swift
+++ Sources/AdventOfCode2020/Day15.swift
@@ -1,8 +1,9 @@
 public enum Day15 {
     struct ElfGame: Sequence, IteratorProtocol {
         let startingNumbers: [Int]
+
         private var turn: Int = 0
-        private var usedNumbers = [Int: [Int]]()
+        private var usedNumbers = [Int: (Int, Int)]()
         private var lastNumber: Int?
 
         init(startingNumbers: [Int]) {
@@ -11,16 +12,12 @@ public enum Day15 {
 
         mutating func next() -> Int? {
             let returnValue: Int
+
             if startingNumbers.indices.contains(turn) {
                 returnValue = startingNumbers[turn]
             } else if let lastNum = lastNumber {
-                if let lastUses = usedNumbers[lastNum], lastUses.count > 1 {
-                    let uses = lastUses.suffix(2)
-                    if let oldest = uses.first, let latest = uses.last {
-                        returnValue = latest - oldest
-                    } else {
-                        returnValue = 0
-                    }
+                if let lastUses = usedNumbers[lastNum] {
+                    returnValue = lastUses.1 - lastUses.0
                 } else {
                     returnValue = 0
                 }
@@ -28,12 +25,13 @@ public enum Day15 {
                 returnValue = 0
             }
 
-            lastNumber = returnValue
-            if let lastUses = usedNumbers[returnValue], let lastUse = lastUses.last {
-                usedNumbers[returnValue] = [lastUse, turn]
+            if let lastUses = usedNumbers[returnValue] {
+                usedNumbers[returnValue] = (lastUses.1, turn)
             } else {
-                usedNumbers[returnValue] = [turn]
+                usedNumbers[returnValue] = (turn, turn)
             }
+
+            lastNumber = returnValue
             turn += 1
 
             return returnValue
@@ -43,20 +41,16 @@ public enum Day15 {
     public static func solvePartOne(from input: String) -> Int {
         let startingNums = input.split(separator: ",").compactMap { Int(String($0)) }
         var x = ElfGame(startingNumbers: startingNums)
-        var value: Int? = 0
-        for _ in 1...2020 {
-            value = x.next()
-        }
-        return value ?? 0
+        return (1...2020)
+            .map({ _ in x.next() ?? 0 })
+            .last ?? 0
     }
 
     public static func solvePartTwo(from input: String) -> Int {
         let startingNums = input.split(separator: ",").compactMap { Int(String($0)) }
         var x = ElfGame(startingNumbers: startingNums)
-        var value: Int? = 0
-        for _ in 1...30000000 {
-            value = x.next()
-        }
-        return value ?? 0
+        return (1...30000000)
+            .map({ _ in x.next() ?? 0 })
+            .last ?? 0
     }
 }
